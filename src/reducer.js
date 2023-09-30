af7498fb07fd585d106d53c04d9f8ce19a002ae9
qkrwnsgk320 <qkrwnsgk320@gmail.com>
2023-09-29 23:55:21 -0700

    ch3.5

--- src/reducer.js
+++ src/reducer.js
@@ -3,7 +3,7 @@ export const initialState = {
   user: null,
 };
 export const getBasketTotal = (basket) =>
-  basekt?.reduce((amount, item) => item.price + amount, 0);
+  basket?.reduce((amount, item) => item.price + amount, 0);
 
 const reducer = (state, action) => {
   console.log(action);
@@ -13,6 +13,27 @@ const reducer = (state, action) => {
         ...state,
         basket: [...state.basket, action.item],
       };
+
+    case "REMOVE_FROM_BASKET":
+      const index = state.basket.findIndex(
+        (basketItem) => basketItem.id === action.id
+      );
+
+      let newBasket = [...state.basket];
+
+      if(index >= 0) {
+          newBasket.splice(index, 1);
+
+      }else{
+        console.warn(
+          'Cant remove product (id: ${action.id}) as its not in basket.'
+        )
+      }
+
+      return {
+        ...state,
+        basket: newBasket
+      }
 
     default:
       return state;
